5cff3c8009fd8aea0c9899881c6c1ce9147398b9
Clupai8o0 <clupaio4@gmail.com>
2024-03-24 18:46:58 +0300

    go go pls

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://more-rooster-25.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
